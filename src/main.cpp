808dcc5d08f22ce5edcf3019d98cdc5390c888aa
DESKTOP-SESPIFB\kaito <s223976x@st.go.tuat.ac.jp>
2025-01-27 14:23:07 +0900

    シリアルで、float値を送るように変更

--- src/main.cpp
+++ src/main.cpp
@@ -5,101 +5,33 @@
 
 FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> can3;
 
-int Shoulder1  = 0;
-int Shoulder2  = 0;
-int Upperarm  = 0;
-int Elbow  = 0;
-int Forearm  = 0;
-int wrist1 = 0;
-int wrist2 = 0;
+float Shoulder1  = 0;
+float Shoulder2  = 0;
+float Upperarm  = 0;
+float Elbow  = 0;
+float Forearm  = 0;
+float wrist1 = 0;
+float wrist2 = 0;
 
 float finger[13]{};
 
 IntervalTimer myTimer;
 void Timtask(){
 
-  PICO_SERIAL.print("SS\t");
-PICO_SERIAL.print(Shoulder1);
-PICO_SERIAL.print("\t");
-PICO_SERIAL.print(Shoulder2);
-PICO_SERIAL.print("\t");
-PICO_SERIAL.print(Upperarm);
-PICO_SERIAL.print("\t");
-PICO_SERIAL.print(Elbow);
-PICO_SERIAL.print("\t");
-PICO_SERIAL.print(Forearm);
-PICO_SERIAL.print("\t");
-PICO_SERIAL.print(wrist1);
-PICO_SERIAL.print("\t");
-PICO_SERIAL.print(wrist2);
-PICO_SERIAL.print("\t");
-  
-  for (int i=0;i<13; i++){
-    PICO_SERIAL.print(finger[i],4);
-    PICO_SERIAL.print("\t");
-  }
-  PICO_SERIAL.println("EE");
-}
-
-
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);delay(400);
-  PICO_SERIAL.begin(115200);
-  can3.begin();
-  can3.setBaudRate(1000000);
-  myTimer.begin(Timtask, 10000);//μs
-
-
-}
-
-void loop() {
-Shoulder1 = analogRead(12)/4.f;
+  Shoulder1 = analogRead(12)/4.f;
 Shoulder2 = analogRead(13)/4.f;
 Upperarm = analogRead(11)/4.f;
 Elbow = analogRead(10)/4.f;
 Forearm = analogRead(A16)/4.f;
 wrist1 = analogRead(A14)/4.f;
 wrist2 = analogRead(A15)/4.f;
-
-
-
 finger[0] = analogRead(7)/4;
 finger[1] = analogRead(5)/4;
 finger[2] = analogRead(3)/4;
 finger[3] = analogRead(1)/4;
 finger[4] = analogRead(A17)/4;
 
-
-// Serial.print("Shoulder1: ");
-// Serial.print(Shoulder1);
-// Serial.print(" Shoulder2: ");
-// Serial.print(Shoulder2);
-// Serial.print(" Upperarm: ");
-// Serial.print(Upperarm);
-// Serial.print(" Elbow: ");
-// Serial.print(Elbow);
-// Serial.print(" Forearm: ");
-// Serial.print(Forearm);
-// Serial.print(" wrist1: ");
-// Serial.print(wrist1);
-// Serial.print(" wrist2: ");
-// Serial.print(wrist2);
-
-// Serial.print("finger[0]: ");
-// Serial.print(finger[0]);
-// Serial.print(" finger[1]: ");
-// Serial.print(finger[1]);
-// Serial.print(" finger[2]: ");
-// Serial.print(finger[2]);
-// Serial.print(" finger[3]: ");
-// Serial.print(finger[3]);
-// Serial.print(" finger[4]: ");
-// Serial.println(finger[4]);
-
-delay(10);
-  // put your main code here, to run repeatedly:
-  CAN_message_t msg1;
+CAN_message_t msg1;
     // msg1.flags.extended = 0;
     msg1.id = 0x111;
     // msg1.len = 5;
@@ -132,4 +64,76 @@ delay(10);
     msg2.buf[7] = 0;
     msg2.buf[8] = 0;
     can3.write(msg2);
+
+  Shoulder1 = -(Shoulder1-70)/53.15775;
+  if(Shoulder1>1.56){
+    Shoulder1=1.56;
+  }else if(Shoulder1<-3.14){
+    Shoulder1 = -3.14;
+  }
+
+  Shoulder2 = -(Shoulder2-159)/53.15775;
+  if(Shoulder2>3.14){
+    Shoulder2=3.14;
+  }else if(Shoulder2<-0.3){
+    Shoulder2 = -0.3;
+  }
+
+  Upperarm = (Upperarm-75)/53.15775;
+  if(Upperarm>2.29){
+    Upperarm=2.29;
+  }else if(Upperarm<-1.53){
+    Upperarm = -1.53;
+  }
+
+  Elbow = (Elbow-255)/44.55;
+  if(Elbow>0){
+    Elbow=0;
+  }else if(Elbow<-2.2){
+    Elbow = -2.2;
+  }
+
+  Forearm = (Forearm -68)/62.39;
+  if(Forearm>1.57){
+    Forearm=1.57;
+  }else if(Forearm<-1.57){
+    Forearm = -1.57;
+  }
+
+  PICO_SERIAL.print("SS\t");
+  PICO_SERIAL.print(Shoulder1,4);
+  PICO_SERIAL.print("\t");
+  PICO_SERIAL.print(Shoulder2,4);
+  PICO_SERIAL.print("\t");
+  PICO_SERIAL.print(Upperarm,4);
+  PICO_SERIAL.print("\t");
+  PICO_SERIAL.print(Elbow,4);
+  PICO_SERIAL.print("\t");
+  PICO_SERIAL.print(Forearm,4);
+  PICO_SERIAL.print("\t");
+  PICO_SERIAL.print(wrist1,4);
+  PICO_SERIAL.print("\t");
+  PICO_SERIAL.print(wrist2,4);
+  PICO_SERIAL.print("\t");
+  
+  for (int i=0;i<13; i++){
+    PICO_SERIAL.print(finger[i],4);
+    PICO_SERIAL.print("\t");
+  }
+  PICO_SERIAL.println("EE");
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);delay(400);
+  PICO_SERIAL.begin(115200);
+  can3.begin();
+  can3.setBaudRate(1000000);
+  myTimer.begin(Timtask, 10000);//μs
+
+
+}
+
+void loop() {
 }
