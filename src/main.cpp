c7a6fb3bda6879462c5246dc97751e3fa7204872
furufuru <furufuruy0503@gmail.com>
2025-09-08 00:04:23 +0900

    いらないものコメントアウト

--- src/main.cpp
+++ src/main.cpp
@@ -31,39 +31,39 @@ void Timtask(){
   finger[3] = analogRead(1)/4.f;
   finger[4] = analogRead(A17)/4.f;
 
-  CAN_message_t msg1;
-    // msg1.flags.extended = 0;
-    msg1.id = 0x111;
-    // msg1.len = 5;
-    // for (int i = 0; i < 5; i++) {
-    //     msg1.buf[i] = data[i];
-    // }
+  // CAN_message_t msg1;
+  //   // msg1.flags.extended = 0;
+  //   msg1.id = 0x111;
+  //   // msg1.len = 5;
+  //   // for (int i = 0; i < 5; i++) {
+  //   //     msg1.buf[i] = data[i];
+  //   // }
     
-    msg1.buf[0] = Shoulder1;
-    msg1.buf[1] = Shoulder2;
-    msg1.buf[2] = Upperarm;
-    msg1.buf[3] = Elbow;
-    msg1.buf[4] = Forearm;
-    msg1.buf[5] = wrist1;
-    msg1.buf[6] = wrist2;
-    msg1.buf[7] = 0;
-    msg1.buf[8] = 0;
+  //   msg1.buf[0] = Shoulder1;
+  //   msg1.buf[1] = Shoulder2;
+  //   msg1.buf[2] = Upperarm;
+  //   msg1.buf[3] = Elbow;
+  //   msg1.buf[4] = Forearm;
+  //   msg1.buf[5] = wrist1;
+  //   msg1.buf[6] = wrist2;
+  //   msg1.buf[7] = 0;
+  //   msg1.buf[8] = 0;
 
-    can3.write(msg1);
+  //   can3.write(msg1);
 
-    delay(2);
+  //   delay(2);
 
-    CAN_message_t msg2;
-    // msg2.flags.extended = 0;
-    msg2.id = 0x222;
-    // msg2.len = 5;
-    for (int i = 0; i < 5; i++) {
-        msg2.buf[i] = finger[i];
-    }
-    msg2.buf[6] = 0;
-    msg2.buf[7] = 0;
-    msg2.buf[8] = 0;
-    can3.write(msg2);
+  //   CAN_message_t msg2;
+  //   // msg2.flags.extended = 0;
+  //   msg2.id = 0x222;
+  //   // msg2.len = 5;
+  //   for (int i = 0; i < 5; i++) {
+  //       msg2.buf[i] = finger[i];
+  //   }
+  //   msg2.buf[6] = 0;
+  //   msg2.buf[7] = 0;
+  //   msg2.buf[8] = 0;
+  //   can3.write(msg2);
 
 
   Shoulder1 = -(Shoulder1-70)/53.15775;
@@ -101,27 +101,27 @@ void Timtask(){
     Forearm = -1.57;
   }
 
-  PICO_SERIAL.print("SS\t");
-  PICO_SERIAL.print(Shoulder1,4);
-  PICO_SERIAL.print("\t");
-  PICO_SERIAL.print(Shoulder2,4);
-  PICO_SERIAL.print("\t");
-  PICO_SERIAL.print(Upperarm,4);
-  PICO_SERIAL.print("\t");
-  PICO_SERIAL.print(Elbow,4);
-  PICO_SERIAL.print("\t");
-  PICO_SERIAL.print(Forearm,4);
-  PICO_SERIAL.print("\t");
-  PICO_SERIAL.print(wrist1,4);
-  PICO_SERIAL.print("\t");
-  PICO_SERIAL.print(wrist2,4);
-  PICO_SERIAL.print("\t");
+  // PICO_SERIAL.print("SS\t");
+  // PICO_SERIAL.print(Shoulder1,4);
+  // PICO_SERIAL.print("\t");
+  // PICO_SERIAL.print(Shoulder2,4);
+  // PICO_SERIAL.print("\t");
+  // PICO_SERIAL.print(Upperarm,4);
+  // PICO_SERIAL.print("\t");
+  // PICO_SERIAL.print(Elbow,4);
+  // PICO_SERIAL.print("\t");
+  // PICO_SERIAL.print(Forearm,4);
+  // PICO_SERIAL.print("\t");
+  // PICO_SERIAL.print(wrist1,4);
+  // PICO_SERIAL.print("\t");
+  // PICO_SERIAL.print(wrist2,4);
+  // PICO_SERIAL.print("\t");
   
-  for (int i=0;i<13; i++){
-    PICO_SERIAL.print(finger[i],4);
-    PICO_SERIAL.print("\t");
-  }
-  PICO_SERIAL.println("EE");
+  // for (int i=0;i<13; i++){
+  //   PICO_SERIAL.print(finger[i],4);
+  //   PICO_SERIAL.print("\t");
+  // }
+  // PICO_SERIAL.println("EE");
 
 
   Serial.print("SS\t");
@@ -139,16 +139,22 @@ void Timtask(){
   Serial.print("\t");
   Serial.print(wrist2,4);
   Serial.print("\t");
+  for (int i=0;i<13; i++){
+    Serial.print(finger[i],4);
+    Serial.print("\t");
+  }
+  Serial.print("EE");
 }
 
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);delay(400);
-  PICO_SERIAL.begin(115200);
-  can3.begin();
-  can3.setBaudRate(1000000);
-  myTimer.begin(Timtask, 10000);//μs
+  Serial.begin(115200);
+  delay(400);
+  // PICO_SERIAL.begin(115200);
+  // can3.begin();
+  // can3.setBaudRate(1000000);
+  myTimer.begin(Timtask, 2000);//μs
 
 
 }
